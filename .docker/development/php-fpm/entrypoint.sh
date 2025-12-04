a16b61038742966b63345cff8b985a94596db660
Jean-Dv-Net <jvalencia@netgrid.com.co>
2025-12-04 15:33:49 -0500

    Remove gosu dependency and `composer install` from php-fpm container, and set container user directly to www-data.

--- .docker/development/php-fpm/entrypoint.sh
+++ .docker/development/php-fpm/entrypoint.sh
@@ -8,15 +8,11 @@ GROUP_ID=${GID:-1000}
 echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
 chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be changed"
 
-# Install composer dependencies as www user (required for development/staging)
-echo "Installing composer dependencies..."
-gosu www composer install --optimize-autoloader --no-interaction --no-progress --prefer-dist
-
 # Clear configurations to avoid caching issues in development
 echo "Clearing configurations..."
-gosu www php artisan config:clear
-gosu www php artisan route:clear
-gosu www php artisan view:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 
 # Run the default command as www user (not root)
-exec gosu www "$@"
+exec "$@"

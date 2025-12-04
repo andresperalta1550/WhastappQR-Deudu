07eb86a5249ea3ab3acde6661537609b1e5784de
Jean-Dv-Net <jvalencia@netgrid.com.co>
2025-12-04 13:29:30 -0500

    feat: Introduce dedicated queue worker services across all environments and add a production Docker Compose configuration.

--- .docker/development/php-fpm/entrypoint.sh
+++ .docker/development/php-fpm/entrypoint.sh
@@ -11,9 +11,9 @@ chown -R ${USER_ID}:${GROUP_ID} /var/www || echo "Some files could not be change
 
 # Clear configurations to avoid caching issues in development
 echo "Clearing configurations..."
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
+su www -c "php artisan config:clear"
+su www -c "php artisan route:clear"
+su www -c "php artisan view:clear"
 
 # Run the default command (e.g., php-fpm or bash)
 exec "$@"
